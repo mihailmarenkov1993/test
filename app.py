b587c5dbf1830a3d7ff481291cc92a8a47a3a89f
Mihail <mihail.marenkov@mail.ru>
2025-08-13 11:57:56 +0300

    add db connection

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("hello world")
+
+git config --global user.name "Mihail"
+git config --global user.email "mihail.marenkov@mail.ru"
+
+git status - проверка статуса
+git add <название файла> - добавление в index
+gid commit -m - "комментарий к коммиту"
+git log - оттобразить коммиты
